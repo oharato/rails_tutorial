5a86d06e180aa4d869c7836484f09d41d6ce06c6
oharato <oharato@live.jp>
2014-06-14 09:12:53 +0000

    test

--- first-app/config/routes.rb
+++ first-app/config/routes.rb
@@ -1,4 +1,6 @@
 FirstApp::Application.routes.draw do
+  resources :posts
+
   resources :microposts
 
   resources :users
--- first-app/db/schema.rb
+++ first-app/db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519142748) do
+ActiveRecord::Schema.define(version: 20140527081509) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "posts", force: true do |t|
+    t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
